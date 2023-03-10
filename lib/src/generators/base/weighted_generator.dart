1cb5d9601d319ddf353d7fa3677fa9fb95c0c791
ItayElf <thh123321@gmail.com>
2023-03-10 21:54:24 +0200

    Feat: added weighted generator

---
+++ lib/src/generators/base/weighted_generator.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+import 'package:radnpg/src/generators/base/generator.dart';
+
+class WeightedGenerator<T> implements IGenerator<T> {
+  /// A map containing the values with their respective probabilities as integers
+  ///
+  /// For example, a map {"a": 3, "b": 1} means "a" has a probability of 3/4 while "b" has a probability of 1/4
+  final Map<T, int> pool;
+  Random random = Random();
+
+  WeightedGenerator(this.pool);
+
+  @override
+  T generate() {
+    int sum = pool.values.toList().reduce((value, element) => value + element);
+    int choice = random.nextInt(sum) + 1;
+
+    for (var pair in pool.entries) {
+      if (pair.value <= choice) {
+        return pair.key;
+      }
+      choice -= pair.value;
+    }
+    throw Exception(
+        "Something went wrong while generating weighted pool $pool");
+  }
+
+  @override
+  void seed(int seed) {
+    random = Random(seed);
+  }
+}
