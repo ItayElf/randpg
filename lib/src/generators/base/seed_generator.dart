170def73296bf7b1133c1cbf232efc150f8503bb
ItayElf <thh123321@gmail.com>
2023-03-10 22:21:15 +0200

    Fix: fixed seed generator max seed

--- lib/src/generators/base/seed_generator.dart
+++ lib/src/generators/base/seed_generator.dart
@@ -2,8 +2,8 @@ import 'dart:math';
 
 /// A class that generates seeds for entities
 class SeedGenerator {
-  /// The maximum seed available, due to javascript's `Number.MAX_SAFE_INT`
-  static const maxSeed = 9007199254740991;
+  /// The maximum seed available
+  static const maxSeed = 4294967296;
 
   /// Generates a random seed
   static int generate() => Random().nextInt(maxSeed);
