05b23dc151457798d4258c3d54ed14c0343cf01e
ItayElf <thh123321@gmail.com>
2023-03-11 21:26:42 +0200

    Feat: added female dragonborn name generator

---
+++ lib/src/generators/names/races/dragonborn/female_dragonborn_name_generator.dart
@@ -0,0 +1,40 @@
+import '../../../base/generator.dart';
+import '../../../base/list_item_genrator.dart';
+import '../../../base/seed_generator.dart';
+import '../../../base/weighted_generator.dart';
+import 'dragonborn_name_data.dart';
+
+/// A class used to generate female dragonborn names
+class FemaleDragonbornNameGenerator implements IGenerator<String> {
+  late int _seed;
+
+  FemaleDragonbornNameGenerator() {
+    _seed = SeedGenerator.generate();
+  }
+
+  static final List<IGenerator<String>> _generatorsTemplate = [
+    ListItemGenerator(dragonbornFemaleNamePrefix),
+    ListItemGenerator(dragonbornFemaleNameSuffix),
+    ListItemGenerator([" "]),
+    ListItemGenerator(dragonbornSurnamePrefix),
+    WeightedGenerator(dragonbornSurnameVowel),
+    ListItemGenerator(dragonbornSurnameMiddle),
+    WeightedGenerator(dragonbornSurnameVowel),
+    ListItemGenerator(dragonbornSurnameSuffix)
+  ];
+
+  @override
+  String generate() {
+    List<IGenerator<String>> generators = List.from(_generatorsTemplate);
+
+    for (int i = 0; i < generators.length; i++) {
+      generators[i].seed((_seed + i) % SeedGenerator.maxSeed);
+    }
+    return generators.map((generator) => generator.generate()).toList().join();
+  }
+
+  @override
+  void seed(int seed) {
+    _seed = seed;
+  }
+}
