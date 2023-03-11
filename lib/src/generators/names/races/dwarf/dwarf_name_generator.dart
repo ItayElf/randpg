52fb9312c19ff21a41d6a91128386c20ae930428
ItayElf <thh123321@gmail.com>
2023-03-11 21:53:40 +0200

    Feat: added dwarf name generator

---
+++ lib/src/generators/names/races/dwarf/dwarf_name_generator.dart
@@ -0,0 +1,27 @@
+import '../../../base/generator.dart';
+import '../../../base/seed_generator.dart';
+import 'female_dwarf_name_generator.dart';
+import 'male_dwarf_name_generator.dart';
+
+class DwarfNameGenerator implements IGenerator<String> {
+  late int _seed;
+  final bool _isMale;
+
+  DwarfNameGenerator(this._isMale) {
+    _seed = SeedGenerator.generate();
+  }
+
+  /// Generates a dwarf name based on its gender
+  @override
+  String generate() {
+    var generator =
+        _isMale ? MaleDwarfNameGenerator() : FemaleDwarfNameGenerator();
+    generator.seed(_seed);
+    return generator.generate();
+  }
+
+  @override
+  void seed(int seed) {
+    _seed = seed;
+  }
+}
