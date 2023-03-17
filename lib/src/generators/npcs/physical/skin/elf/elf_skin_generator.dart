4c536ce1fbb5e846cace0806d291f9f80ca5ea48
ItayElf <thh123321@gmail.com>
2023-03-17 20:26:08 +0200

    Feat: added elven skin generator

---
+++ lib/src/generators/npcs/physical/skin/elf/elf_skin_generator.dart
@@ -0,0 +1,37 @@
+import '../../../../base/generator.dart';
+import '../../../../base/list_item_generator.dart';
+import '../../../../base/seed_generator.dart';
+
+/// A class that generates elven skin tones
+class ElfSkinGenerator implements IGenerator<String> {
+  late int _seed;
+
+  static const _skinTones = [
+    "white",
+    "pale",
+    "brown",
+    'blueish',
+    'pink',
+    'golden',
+    'silver',
+    'green',
+    'copper'
+  ];
+
+  ElfSkinGenerator() {
+    _seed = SeedGenerator.generate();
+  }
+
+  /// Generates an elven skin tone
+  @override
+  String generate() {
+    final generator = ListItemGenerator(_skinTones);
+    generator.seed(_seed);
+    return generator.generate();
+  }
+
+  @override
+  void seed(int seed) {
+    _seed = seed;
+  }
+}
