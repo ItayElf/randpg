629bfb42e966e1d4911959a3353ed593d8765684
ItayElf <thh123321@gmail.com>
2023-03-17 14:32:56 +0200

    Fix: changed dragonborn bool to gender

--- lib/src/generators/names/races/dragonborn/dragonborn_name_generator.dart
+++ lib/src/generators/names/races/dragonborn/dragonborn_name_generator.dart
@@ -1,3 +1,5 @@
+import '../../../../enums/gender.dart';
+import '../../../../randpg_exceptions.dart';
 import '../../../base/generator.dart';
 import '../../../base/seed_generator.dart';
 import 'female_dragonborn_name_generator.dart';
@@ -6,20 +8,30 @@ import 'male_dragonborn_name_generator.dart';
 /// A generator for dragonborn names
 class DragonbornNameGenerator implements IGenerator<String> {
   late int _seed;
-  final bool _isMale;
+  final Gender _gender;
 
-  DragonbornNameGenerator(this._isMale) {
+  DragonbornNameGenerator(this._gender) {
     _seed = SeedGenerator.generate();
   }
 
   /// Generates a dragonborn name based on its gender
   @override
   String generate() {
-    var generator = _isMale
-        ? MaleDragonbornNameGenerator()
-        : FemaleDragonbornNameGenerator();
+    var generator = _getGenerator(_gender);
     generator.seed(_seed);
     return generator.generate();
+  }
+
+  /// Returns the correct generator based on the gender
+  IGenerator<String> _getGenerator(Gender gender) {
+    switch (gender) {
+      case Gender.male:
+        return MaleDragonbornNameGenerator();
+      case Gender.female:
+        return FemaleDragonbornNameGenerator();
+    }
+    // ignore: dead_code
+    throw GenderNotSupportedException("Gender ${gender.name} is not supported");
   }
 
   @override
