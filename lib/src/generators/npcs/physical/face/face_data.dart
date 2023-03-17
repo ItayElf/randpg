1821808aec9bf47ad3bb99f68e05fd78b7381705
ItayElf <thh123321@gmail.com>
2023-03-17 21:44:44 +0200

    Feat: added face generator

---
+++ lib/src/generators/npcs/physical/face/face_data.dart
@@ -0,0 +1,42 @@
+import '../../../../enums/race.dart';
+
+const faceShape = [
+  'sharp',
+  'round',
+  'oval',
+  'soft',
+  'edgy',
+  'square',
+];
+
+const goodLookingFace = [
+  'cute',
+  'attractive',
+  'beautiful',
+  'glamorous',
+  'innocent',
+  'stunning',
+  'fanciable',
+  'pretty',
+  'magnificent',
+  'sightly'
+];
+
+const regularLookingFace = [
+  'average',
+  'bland',
+  'unremarkable',
+  'forgettable',
+  'typical',
+  'common',
+  'ordinary',
+  'ugly',
+  'hideous',
+  'disfigured',
+  'disgusting',
+  'creepy'
+];
+
+const faceMap = {
+  [Race.elf]: goodLookingFace,
+};
