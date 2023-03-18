7a60a4e39aa6494279b9447deca5103f2d98d2fd
ItayElf <thh123321@gmail.com>
2023-03-18 12:27:59 +0200

    Feat: added personality generator

--- lib/generators/npcs.dart
+++ lib/generators/npcs.dart
@@ -1,2 +1,5 @@
 export '../src/generators/npcs/physical/physical_description_generator.dart'
     show PhysicalDescriptionGenerator;
+
+export '../src/generators/npcs/personality/personality_generator.dart'
+    show PersonalityGenerator;
---
+++ lib/src/generators/npcs/personality/personality_generator.dart
@@ -0,0 +1,40 @@
+import '../../../entities/npcs/alignment.dart';
+import '../../../entities/npcs/personality.dart';
+import '../../../enums/race.dart';
+import '../../base/batch_generator.dart';
+import '../../base/generator.dart';
+import '../../base/seed_generator.dart';
+import 'alignment/alignment_generator.dart';
+import 'quirks/personality_quirks_generator.dart';
+import 'traits/personality_traits_generator.dart';
+
+/// A class that generates personalities
+class PersonalityGenerator implements IGenerator<Personality> {
+  late int _seed;
+  final Race _race;
+
+  PersonalityGenerator(this._race) {
+    _seed = SeedGenerator.generate();
+  }
+
+  /// Generates a personality based on [_race]
+  @override
+  Personality generate() {
+    final generator = BatchGenerator({
+      "alignment": AlignmentGenerator(_race),
+      "traits": PersonalityTraitsGenerator(_race),
+      "quirks": PersonalityQuirksGenerator(_race),
+    });
+    generator.seed(_seed);
+
+    final result = generator.generate();
+    result["alignment"] = (result["alignment"] as Alignment).toMap();
+
+    return Personality.fromMap(result);
+  }
+
+  @override
+  void seed(int seed) {
+    _seed = seed;
+  }
+}
