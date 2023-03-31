6fa295d42322ce5016d878c5e43871064aef1664
ItayElf <thh123321@gmail.com>
2023-03-31 14:12:11 +0300

    Feat: added half elf race

--- lib/src/races/races.dart
+++ lib/src/races/races.dart
@@ -1,14 +1,20 @@
-import 'package:randpg/src/races/gnome/gnome.dart';
-
 import 'dragonborn/dragonborn.dart';
 import 'dwarf/dwarf.dart';
 import 'elf/elf.dart';
+import 'gnome/gnome.dart';
 import 'human/human.dart';
+import 'hybrid/hybrid.dart';
 
 const availableRaces = [
   Dragonborn(),
   Dwarf(),
   Elf(),
   Gnome(),
+  Hybrid(
+    firstRace: Human(),
+    secondRace: Elf(),
+    raceName: "Half-elf",
+    pluralRaceName: "Half-elves",
+  ),
   Human(),
 ];
