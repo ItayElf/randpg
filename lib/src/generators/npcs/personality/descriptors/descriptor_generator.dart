93200bb618e58669d76dc7544d9c52c2d76c3ece
ItayElf <thh123321@gmail.com>
2023-03-31 17:01:30 +0300

    Feat: added descriptor generator

---
+++ lib/src/generators/npcs/personality/descriptors/descriptor_generator.dart
@@ -0,0 +1,7 @@
+import '../../../base/list_item_generator.dart';
+import 'descriptors_data.dart';
+
+/// A generator for characters descriptors
+class DescriptorGenerator extends ListItemGenerator<String> {
+  DescriptorGenerator() : super(descriptors);
+}
