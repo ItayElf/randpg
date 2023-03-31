80a98d8cee89a41d305f216c8fda5c9b5e3d898f
ItayElf <thh123321@gmail.com>
2023-03-31 19:52:07 +0300

    Feat: added npc dataclass

--- lib/entities/npcs.dart
+++ lib/entities/npcs.dart
@@ -3,3 +3,4 @@ export '../src/entities/npcs/hair.dart' show Hair;
 export '../src/entities/npcs/personality.dart' show Personality;
 export '../src/entities/npcs/physical_description.dart'
     show PhysicalDescription;
+export '../src/entities/npcs/npc.dart' show Npc;
