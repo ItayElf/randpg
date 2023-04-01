d958976236d42d1d0045c975bad87c4341bfe966
ItayElf <thh123321@gmail.com>
2023-04-01 18:36:13 +0300

    Feat: added settlement entity

--- lib/entities/settlements.dart
+++ lib/entities/settlements.dart
@@ -1,2 +1,3 @@
 export '../src/entities/settlements/goods.dart' show Goods;
 export '../src/entities/settlements/location.dart' show Location;
+export '../src/entities/settlements/settlement.dart' show Settlement;
