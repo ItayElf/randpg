aa43694e0d04606bbee4d043dc0c64bf502afc52
ItayElf <thh123321@gmail.com>
2023-04-03 07:08:37 +0300

    Feat: added settlement name generators

---
+++ lib/src/generators/settlements/names/domintant_race_name_generator.dart
@@ -0,0 +1,38 @@
+import '../../../enums/gender.dart';
+import '../../../subtypes/races/race.dart';
+import '../../../subtypes/races/race_manager.dart';
+import '../../base/generator.dart';
+import '../../base/list_item_generator.dart';
+import '../../base/seed_generator.dart';
+
+/// A class that generates town names based on race names
+class DominantRaceNameGenerator implements IGenerator<String> {
+  late int _seed;
+  Race? _dominantRace;
+
+  DominantRaceNameGenerator(this._dominantRace) {
+    _seed = SeedGenerator.generate();
+  }
+
+  @override
+  String generate() {
+    final genderGenerator = ListItemGenerator(Gender.values);
+    genderGenerator.seed(_seed);
+    final gender = genderGenerator.generate();
+
+    if (_dominantRace == null) {
+      final raceGenerator = ListItemGenerator(RaceManager.activeRaces);
+      raceGenerator.seed((_seed + 1) % SeedGenerator.maxSeed);
+      _dominantRace = raceGenerator.generate();
+    }
+
+    final generator = _dominantRace!.getNameGenerator(gender);
+    generator.seed((_seed + 2) % SeedGenerator.maxSeed);
+    return generator.generate().split(" ").first;
+  }
+
+  @override
+  void seed(int seed) {
+    _seed = seed;
+  }
+}
