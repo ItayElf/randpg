239a9dd8b2cf30e785645aba974958b5cfad3de0
ItayElf <thh123321@gmail.com>
2023-04-05 14:21:51 +0300

    Feat: added exports

--- lib/subtypes/settlements.dart
+++ lib/subtypes/settlements.dart
@@ -1,1 +1,5 @@
 export '../src/subtypes/settlements/settlement_type.dart' show SettlementType;
+export '../src/subtypes/settlements/settlements_manager.dart'
+    show SettlementManager;
+
+export '../src/subtypes/settlements/hamlet/hamlet.dart' show Hamlet;
