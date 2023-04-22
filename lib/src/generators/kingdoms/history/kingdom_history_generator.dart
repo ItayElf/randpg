3e2d6100338f3f394ae1f93db9acaf9b86b3924c
ItayElf <thh123321@gmail.com>
2023-04-22 14:53:47 +0300

    Fix: added newlines to kingdom history

--- lib/src/generators/kingdoms/history/kingdom_history_generator.dart
+++ lib/src/generators/kingdoms/history/kingdom_history_generator.dart
@@ -29,13 +29,13 @@ class KingdomHistoryGenerator implements IGenerator<String> {
   }
 
   String _getHistory(Map<String, dynamic> map) =>
-      "${titledEach(_kingdomName)} is ${article(map["age"])} kingdom that rose to power through ${map["rise"]}. "
-      "${titledEach(_kingdomName)} faced ${map["challenge"]}, which ${map["result"]}. "
+      "${titledEach(_kingdomName)} is ${article(map["age"])} kingdom that rose to power through ${map["rise"]}.\n"
+      "${titledEach(_kingdomName)} faced ${map["challenge"]}, which ${map["result"]}.\n"
       "Today, ${titledEach(_kingdomName)} is ${map["state"]}.";
 
   String _getAlternativeFormatHistory(Map<String, dynamic> map) =>
       "Having risen to prominence through ${map["rise"]}, ${titledEach(_kingdomName)}, ${article(map["age"])} "
-      "kingdom, was confronted by ${map["challenge"]}, which ${map["result"]}. "
+      "kingdom, was confronted by ${map["challenge"]}, which ${map["result"]}.\n"
       "Currently, ${titledEach(_kingdomName)} is ${map["state"]}";
 
   Map<String, IGenerator> _getBatch() => {
