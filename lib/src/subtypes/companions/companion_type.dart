9a9d8ee383813a8cc86716f2d11175453fcfff87
ItayElf <thh123321@gmail.com>
2024-03-29 01:48:59 +0200

    Feat: added companion type

---
+++ lib/src/subtypes/companions/companion_type.dart
@@ -0,0 +1,26 @@
+import '../../enums/gender.dart';
+import '../../generators/base/generator.dart';
+
+/// A class that represents a generic companion type
+abstract class CompanionType {
+  /// Returns a generator for the name of the companion
+  Generator<String> getNameGenerator(Gender gender);
+
+  /// Returns a generator for the gender of the companion
+  Generator<Gender> getGenderGenerator();
+
+  /// Returns the name of the companion type
+  String getCompanionType();
+
+  /// Returns a generator for the appearance of the companion
+  Generator<String> getAppearanceGenerator(String name);
+
+  /// Returns a generator for the personality of the companion
+  Generator<String> getPersonalityGenerator(String name);
+
+  /// Returns a generator for the skills of the companion
+  Generator<List<String>> getSkillsGenerator();
+
+  /// Returns a generator for the quirks of the companion
+  Generator<List<String>> getQuirksGenerator();
+}
