ed9121adffd68a516c49d50b88293bf55592ab74
ItayElf <thh123321@gmail.com>
2024-03-29 11:40:15 +0300

    Feat: added companion to exports

---
+++ lib/entities/companions.dart
@@ -0,0 +1,5 @@
+export '../src/entities/companions/companion.dart' show Companion;
+
+export '../src/subtypes/companions/companion_type.dart' show CompanionType;
+export '../src/subtypes/companions/companion_manager.dart'
+    show CompanionManager;
