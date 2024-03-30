c09ba13f85d86efc8d76f2be8132567c7a60ecde
ItayElf <thh123321@gmail.com>
2024-03-30 12:27:39 +0300

    Feat: exported the companion personality generator

---
+++ lib/entities/custom/custom_companions.dart
@@ -0,0 +1,4 @@
+// A file for exposing all the base generators for creating custom companions
+
+export '../../src/generators/companions/personality/companion_personality_generator.dart'
+    show CompanionPersonalityGenerator;
